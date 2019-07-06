6b50975e3364ad981b2ccbb3aa57a7793f32766d
manideep323 <manideep323@.com>
2019-07-06 11:24:03 +0530

    initial commit

---
+++ src/main/java/com/fpix/TreeStructureApplication.java
@@ -0,0 +1,13 @@
+package com.fpix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeStructureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fpix/service/IIndexService.java
@@ -0,0 +1,7 @@
+package com.fpix.service;
+
+import org.json.simple.JSONArray;
+
+public interface IIndexService {
+	JSONArray readFloderStructure(String flodersPath);
+}
